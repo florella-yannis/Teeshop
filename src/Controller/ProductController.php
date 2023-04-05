cfa686d2331d1f262e8385b16fe33eaf915d49ab
florella-yannis <florellayannis@gmail.com>
2023-04-05 16:58:18 +0200

    create AdminController + create dashboard +make form product

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductController extends AbstractController
+{
+    #[Route('/admin/ajouter-un-produit', name: 'create_product', methods:['GET', 'POST'])]
+    public function createProduct(Request $request, ProductRepository $repo): Response
+    {
+        $product = new Product();
+
+        
+        return $this->render('', [
+            '' => '',
+        ]);
+    }
+}
