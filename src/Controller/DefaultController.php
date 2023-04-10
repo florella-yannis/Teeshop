793c6c0ea2b599c8dea66ce33021f88c16c16665
florella-yannis <florellayannis@gmail.com>
2023-04-10 06:36:02 +0200

    default and showhome *

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'show_home', methods:['GET'])]
-    public function showHome(): Response
+    public function showHome(ProductRepository $repository): Response
     {
-        return $this->render('default/show_home.html.twig');
+        $products = $repository->findBy(['deletedAt' => null]);
+
+        return $this->render('default/show_home.html.twig', [
+            'products' => $products
+        ]);
     }
 }
